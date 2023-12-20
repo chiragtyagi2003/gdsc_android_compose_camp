96b117e9ff0f6ac3dbddabcfd09979bab8d24872
Chirag Tyagi <tyagichirag06@gmail.com>
2023-12-20 05:57:40 +0530

    splash screens

---
+++ app/src/main/java/com/example/buzzz/ui/theme/NavGraph.kt
@@ -0,0 +1,29 @@
+package com.example.buzzz.ui.theme
+
+// NavGraph.kt
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.buzzz.SplashScreen1
+import com.example.buzzz.SplashScreen2
+import com.example.buzzz.SplashScreen3
+
+@Composable
+fun AppNavigation(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "splashScreen1"
+    ) {
+        composable("splashScreen1") {
+            SplashScreen1(navController)
+        }
+        composable("splashScreen2") {
+            SplashScreen2(navController)
+        }
+        composable("splashScreen3") {
+            SplashScreen3(navController)
+        }
+    }
+}
